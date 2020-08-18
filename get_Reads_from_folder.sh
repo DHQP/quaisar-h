215c9b81d2bba9b0bde56022bc6944210eb334db
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-08-18 11:59:45 -0400

    fixed output dir

--- get_Reads_from_folder.sh
+++ get_Reads_from_folder.sh
@@ -15,9 +15,9 @@ fi
 #
 # Description: Will find all fastq.gz files within the given folder. It will move and rename them to the location that the pipeline will expect
 #
-# Usage: ./get_Reads_from_folder.sh full_path_Output_directory folder_with_fastqs postfix_for_reads(1:_SX_L001_RX_00X.fastq.gz 2: _(R)X.fastq.gz 3: _RX_00X.fastq.gz 4: _SX_RX_00X.fastq.gz)
+# Usage: ./get_Reads_from_folder.sh run_ID folder_with_fastqs postfix_for_reads(1:_SX_L001_RX_00X.fastq.gz 2: _(R)X.fastq.gz 3: _RX_00X.fastq.gz 4: _SX_RX_00X.fastq.gz) output_directory
 #
-# Output location: output_location
+# Output location: output_directory/run_ID
 #
 # Modules required: None
 #
@@ -36,7 +36,7 @@ elif [[ -z "${1}" ]]; then
 	echo "Empty project name supplied to $0, exiting"
 	exit 1
 elif [[ "${1}" = "-h" ]]; then
-	echo "Usage is ./get_Reads_from_folder.sh  full_path_Output_directory location_of_fastqs postfix_for_reads( 1: _SX_L001_RX_00X.fastq.gz 2: _(R)X.fastq.gz 3: _RX_00X.fastq.gz 4: _SX_RX_00X.fastq.gz)"
+	echo "Usage is ./get_Reads_from_folder.sh  run_ID location_of_fastqs postfix_for_reads( 1: _SX_L001_RX_00X.fastq.gz 2: _(R)X.fastq.gz 3: _RX_00X.fastq.gz 4: _SX_RX_00X.fastq.gz)"
 	echo "Output by default is downloaded to ${processed}/run_ID and extracted to ${processed}/run_ID/sample_name/FASTQs"
 	exit 0
 elif [[ -z "${2}" ]]; then
@@ -45,16 +45,19 @@ elif [[ -z "${2}" ]]; then
 elif ! [[ ${3} =~ $number ]] || [[ -z "${3}" ]]; then
 	echo "postfix is not a number or is empty. Please input max number of concurrent qsub submissions...exiting"
 	exit 2
+elif ! [[ ! -d ${4} ]]; then
+	echo "Output directory doesnt exist...exiting"
+	exit 2
 fi
 
-if [[ "${3}" -gt 4 ]]; then
+if [[ "${3}" -gt 4 ]] || [[ "${3}" -lt 1 ]]; then
 	echo "postfix for reads is TOO high, only 4 options...1:_SX_L001_RX_00X.fastq.gz 2: _(R)X.fastq.gz 3: _RX_00X.fastq.gz 4: _SX_RX_00X.fastq.gz , exiting"
 	exit
 fi
 
 
 # Sets folder to where files will be downloaded to
-OUTDATADIR="${1}"
+OUTDATADIR="${4}/${1}"
 if [ ! -d "${OUTDATADIR}" ]; then
 	echo "Creating $OUTDATADIR"
 	mkdir -p "${OUTDATADIR}"
