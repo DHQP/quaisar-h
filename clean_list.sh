b143017c0ef8ac778aaa1db3baf17ff72ea64833
nvlachos <6909517+nvlachos@users.noreply.github.com>
2020-09-02 09:29:38 -0400

    needed for OA

--- clean_list.sh
+++ clean_list.sh
@@ -6,16 +6,10 @@
 #$ -cwd
 #$ -q short.q
 
-#Import the config file with shortcuts and settings
-if [[ ! -f "./config.sh" ]]; then
-	cp ./config_template.sh ./config.sh
-fi
-. ./config.sh
-
 #
 # Description: Script to clean any list file of extra newlines and space
 #
-# Usage ./clean_list.sh path_to_list_file
+# Usage ./clean_list.sh -l path_to_list_file
 #
 # Output location: same folder as path_to_list
 #
@@ -26,37 +20,63 @@ fi
 # Created by Nick Vlachos (nvx4@cdc.gov)
 #
 
-# Checks for proper argumentation
-if [[ $# -eq 0 ]]; then
-	echo "No argument supplied to $0, exiting"
+
+show_help () {
+	echo "Usage is ./clean_list.sh -l path_to_list_file"
+}
+
+# Parse command line options
+options_found=0
+while getopts ":h?l:" option; do
+	options_found=$(( options_found + 1 ))
+	case "${option}" in
+		\?)
+			echo "Invalid option found: ${OPTARG}"
+      show_help
+      exit 0
+      ;;
+		l)
+			echo "Option -l triggered, argument = ${OPTARG}"
+			list=${OPTARG};;
+		:)
+			echo "Option -${OPTARG} requires as argument";;
+		h)
+			show_help
+			exit 0
+			;;
+	esac
+done
+
+# Show help info for when no options are given
+if [[ "${options_found}" -eq 0 ]]; then
+	echo "No options found"
+	show_help
+	exit
+fi
+
+if [[ -z "${list}" ]] || [[ ! -f ${list} ]]; then
+	echo "Empty list supplied to clean_list.sh or list file does not exist, exiting"
 	exit 1
-elif [[ -z "${1}" ]] || [[ ! -f ${1} ]]; then
-	echo "Empty sample name supplied to clean_list.sh or list file does not exist, exiting"
-	exit 1
-# Gives the user a brief usage and help section if requested with the -h option argument
-elif [[ "${1}" = "-h" ]]; then
-	echo "Usage is ./clean_list.sh  path_to_list"
-	exit 0
 fi
 
 # Makes a backup of original list file
-cp -f ${1} ${1}.original
+cp -f ${list} ${list}.original
 
 # Puts list through dos2unix to convert any windows line returns to unix
-dos2unix ${1}.original
+dos2unix ${list}.original
 
-tr -d ' \t\r\f' < ${1}.original > ${1}
-ex -s +'v/\S/d' -cwq ${1}
+tr -d ' \t\r\f' < ${list}.original > ${list}
+ex -s +'v/\S/d' -cwq ${list}
 
 # Shows all old lines from list
 while IFS= read -r line; do
 	echo "O:${line}:"
-done < "${1}.original"
+done < "${list}.original"
 
 # Shows all new lines in list
 while IFS= read -r line; do
 	echo "N:${line}:"
-done < "${1}"
+done < "${list}"
 
 #Script exited gracefully (unless something else inside failed)
 exit 0
